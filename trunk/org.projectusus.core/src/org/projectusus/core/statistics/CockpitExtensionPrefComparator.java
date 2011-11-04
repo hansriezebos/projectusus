de354779f143b8ae349442aebf163520e60f5334
mphilipp82 <mphilipp82@395735b6-a3a5-11de-b1fe-bba1d6bec496>
2011-11-04 20:31:46 +0000

    Moved source code to GitHub

--- trunk/org.projectusus.core/src/org/projectusus/core/statistics/CockpitExtensionPrefComparator.java
+++
@@ -1,9 +0,0 @@
-package org.projectusus.core.statistics;
-
-import java.util.Comparator;
-
-final class CockpitExtensionPrefComparator implements Comparator<CockpitExtensionPref> {
-    public int compare( CockpitExtensionPref first, CockpitExtensionPref second ) {
-        return first.getLabel().compareTo( second.getLabel() );
-    }
-}
